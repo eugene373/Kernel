25753abccaf75765754d9324aab2653f0c024c2b
eugene373 <eugene373@gmail.com>
2013-01-09 14:33:24 -0500

    First Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 11 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#11 SMP PREEMPT Wed Jan 9 04:20:17 EST 2013"
+#define LINUX_COMPILE_BY "eugene373"
+#define LINUX_COMPILE_HOST "eugene373"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
